86ed564e72a2bb3f8c59f97c86e4b3f4f5fc64be
thryec <thryec@gmail.com>
2022-01-07 17:30:25 +0800

    successfully ran tests for name and symbol

--- test/Simple777Token.test.js
+++ test/Simple777Token.test.js
@@ -1,49 +1,25 @@
-// Based on https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/test/examples/SimpleToken.test.js
-const {
-  expectEvent,
-  singletons,
-  constants,
-} = require("@openzeppelin/test-helpers");
-const { ZERO_ADDRESS } = constants;
+const { expect } = require("chai");
 
-const Simple777Token = artifacts.require("Simple777Token");
+const ERC777 = artifacts.require("Simple777Token");
 
-contract("Simple777Token", function ([_, registryFunder, creator, operator]) {
-  beforeEach(async function () {
-    this.erc1820 = await singletons.ERC1820Registry(registryFunder);
-    this.token = await Simple777Token.new({ from: creator });
+// has a name
+// has a symbol
+// assigns the initial total supply to the creator
+// allows operator burn
+
+contract("Token Test", async (accounts) => {
+  let contract;
+  beforeEach(async () => {
+    contract = await ERC777.deployed();
   });
 
-  it("has a name", async function () {
-    (await this.token.name()).should.equal("Simple777Token");
+  it("has a name", async () => {
+    const name = await contract.name();
+    expect(name).to.equal("Simple777Token");
   });
 
-  it("has a symbol", async function () {
-    (await this.token.symbol()).should.equal("S7");
-  });
-
-  it("assigns the initial total supply to the creator", async function () {
-    const totalSupply = await this.token.totalSupply();
-    const creatorBalance = await this.token.balanceOf(creator);
-
-    creatorBalance.should.be.bignumber.equal(totalSupply);
-
-    await expectEvent.inConstruction(this.token, "Transfer", {
-      from: ZERO_ADDRESS,
-      to: creator,
-      value: totalSupply,
-    });
-  });
-
-  it("allows operator burn", async function () {
-    const creatorBalance = await this.token.balanceOf(creator);
-    const data = web3.utils.sha3("Simple777Data");
-    const operatorData = web3.utils.sha3("Simple777OperatorData");
-
-    await this.token.authorizeOperator(operator, { from: creator });
-    await this.token.operatorBurn(creator, creatorBalance, data, operatorData, {
-      from: operator,
-    });
-    (await this.token.balanceOf(creator)).should.be.bignumber.equal("0");
+  it("has a symbol", async () => {
+    const symbol = await contract.symbol();
+    expect(symbol).to.equal("S7");
   });
 });
