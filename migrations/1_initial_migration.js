ded850a683be0312810f4d644b3457aecde19d11
thryec <thryec@gmail.com>
2022-01-13 14:13:17 +0800

    successfully deployed crowdsale contract

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -6,28 +6,28 @@ module.exports = async function (deployer, network, accounts) {
   let erc1363Address;
   let crowdsaleTokenAddress;
 
-  console.log("accounts: ", accounts);
-  deployer
+  await deployer
     .deploy(ERC1363, "ERC1363Token", "ERC1363", accounts[0], 10000)
     .then((erc1363) => {
       erc1363Address = erc1363.address;
-      console.log("erc1363 address: ", erc1363Address);
     });
 
-  deployer.deploy(CrowdsaleToken, 10000).then((crwd) => {
+  await deployer.deploy(CrowdsaleToken, 10000).then((crwd) => {
     crowdsaleTokenAddress = crwd.address;
-    console.log("crwd address: ", crowdsaleTokenAddress);
   });
 
-  // deployer
-  //   .deploy(
-  //     CrowdsaleContract,
-  //     10,
-  //     accounts[0],
-  //     crowdsaleTokenAddress,
-  //     erc1363Address
-  //   )
-  //   .then((crowdsaleContract) => {
-  //     console.log("crowdsaleContract: ", crowdsaleContract.address);
-  //   });
+  // console.log("erc1363 address: ", erc1363Address);
+  // console.log("crwd address: ", crowdsaleTokenAddress);
+
+  await deployer
+    .deploy(
+      CrowdsaleContract,
+      10,
+      accounts[0],
+      crowdsaleTokenAddress,
+      erc1363Address
+    )
+    .then((crowdsaleContract) => {
+      console.log("crowdsaleContract: ", crowdsaleContract.address);
+    });
 };
