030d3147a71a51522724f8e739dd14ebf2308298
thryec <thryec@gmail.com>
2022-01-18 15:32:50 +0800

    migrated per token royalty contract

--- contracts/ERC721PerTokenRoyalties.sol
+++ contracts/ERC721PerTokenRoyalties.sol
@@ -9,7 +9,7 @@ import './ERC2981PerTokenRoyalties.sol';
 /// @title Example of ERC721 contract with ERC2981
 /// @author Simon Fremaux (@dievardump)
 /// @notice This is a mock, mint and mintBatch are not protected. Please do not use as-is in production
-contract ERC721WithRoyalties is ERC721, ERC2981PerTokenRoyalties {
+contract ERC721PerTokenRoyalties is ERC721, ERC2981PerTokenRoyalties {
     uint256 nextTokenId;
 
     constructor(string memory name_, string memory symbol_)
--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,9 +1,15 @@
 const ERC721 = artifacts.require("ERC721ContractWideRoyalties");
+const ERC721PerToken = artifacts.require("ERC721PerTokenRoyalties");
 
 module.exports = async function (deployer, network, accounts) {
   await deployer
     .deploy(ERC721, "ERC721WithRoyalties", "ERC721")
-    .then((erc721) => {
-      // console.log("ERC721 deployed to: ", erc721.address);
+    .then((contract) => {
+      console.log("ERC721 deployed to: ", contract.address);
+    });
+  await deployer
+    .deploy(ERC721PerToken, "ERC721PerTokenRoyalties", "ERC721v2")
+    .then((contract) => {
+      console.log("ERC721PerTokenRoyalties deployed to: ", contract.address);
     });
 };
